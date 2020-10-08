4b8e72f8e9079926d61ebeb351e45b4abaed1aa8
Marco Chavez <marcode.chavez.jr@gmail.com>
2020-10-08 15:04:03 -0700

    changing entry to tsx file

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const config = require('./webpack.config');
 
 module.exports = merge(config, {
     mode: 'production',
-    entry: './src/components/react-hls-player.js',
+    entry: './src/components/react-hls-player.tsx',
     devtool: 'source-map',
     externals: {
         'react': 'commonjs react'
